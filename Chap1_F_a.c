c971dc11a401a9c1b83c8f405ade957e283ba7d5
A_Porey <aninditaporey002@gmail.com>
2020-06-11 01:10:42 +0530

    second commit

--- Chap1_F_a.c
+++ Chap1_F_a.c
@@ -6,12 +6,12 @@ int main()
     int BS, DA, HRA, GS ;
     printf("Basic salary of Ramesh is ");
     scanf("%d", & BS);
-    /* Calculation of dearness allowence DA */
+    /* Calculation of dearness allowence */
     DA = BS*40/100;
-    /* Calculation of house rent allowence, HRA */
+    /* Calculation of house rent allowence */
     HRA = BS*20/100;
-    /* Calculation of gross calary, GS */
-    GS = BS+DA+HRA;
+    /* Calculation of gross salary */
+    GS = BS + DA + HRA;
     printf("The gross salary of Ramesh is %d ", GS);
     getch();
     return 0;
